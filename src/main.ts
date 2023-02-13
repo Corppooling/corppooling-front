08f1eee93d4cda360d4f848ce1b9e253083ace00
CesarWatrin <cesar.watrin@hotmail.com>
2023-02-13 12:23:14 +0100

    setup: font awesome setup

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,17 @@ import router from "./router";
 import PrimeVue from "primevue/config";
 import "./assets/index.css";
 
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(PrimeVue);
-
+app.component("FontAwesomeIcon", FontAwesomeIcon);
 app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>Homepage</div>
+  <div class="bg-blue-300">
+    <font-awesome-icon icon="fa-solid fa-phone" />
+    Hello world !
+  </div>
 </template>
