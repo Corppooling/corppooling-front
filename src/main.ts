a3c753a22653f101342b5d6a3b18f9fd5d146aea
CesarWatrin <cesar.watrin@hotmail.com>
2023-02-14 15:21:18 +0100

    feat: adding design system to tailwind config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
-import "@/assets/index.css";
+import "@/assets/styles/main.scss";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 import { createI18n, useI18n } from "vue-i18n";
 import fr from "@/lang/fr.json";
 import en from "@/lang/en.json";
@@ -19,11 +23,6 @@ const i18n = createI18n({
   },
 });
 
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fas, far, fab);
 
 const app = createApp(App, {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="bg-blue-300">
+  <div class="bg-content-flight">
+    <h1>Test</h1>
     <font-awesome-icon icon="fa-solid fa-phone" />
     {{ $t("global.hello") }}
   </div>
