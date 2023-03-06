7330cd16b20ef8105f11f986ce3ae96ea34b66f9
CesarWatrin <cesar.watrin@hotmail.com>
2023-03-06 12:55:07 +0100

    feat: wip home page

---
+++ src/modules/trips/components/molecules/TripCard.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import type { Trip } from "@/interfaces/trip.interface";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { DateTime } from "luxon";
+
+const props = defineProps<{
+  trip: Trip;
+}>();
+</script>
+
+<template>
+  <div class="bg-content-xlight">
+    <div class="bg-white flex">
+      <div class="flex flex-col">
+        <div>
+          <font-awesome-icon icon="fa-calendar" />
+          <span class="ml-1">{{
+            DateTime.fromISO(props.trip.departure_time).toLocaleString({
+              month: "long",
+              day: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
+            })
+          }}</span>
+        </div>
+        <div>
+          <font-awesome-icon icon="fa-map-marker-alt" />
+          <span class="ml-1">{{ props.trip.departure_location }}</span>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,43 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Title from "@/components/atoms/Title.vue";
+import { type Trip, TripType } from "@/interfaces/trip.interface";
+import type { User } from "@/interfaces/user.interface";
+import TripCard from "@/modules/trips/components/molecules/TripCard.vue";
+
+const user: User = {
+  id: 1,
+  firstName: "Maxime",
+  lastName: "Didier",
+  email: "maxime.didier@aol.com",
+  profile_image:
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffr.linkedin.com%2Fin%2Fmaxime-didier-8b25751ba&psig=AOvVaw0IJelvHtNEhLdPF0unLVh6&ust=1678189002082000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCJCskK-bx_0CFQAAAAAdAAAAABAE",
+};
+
+const trips: Array<Trip> = [
+  {
+    id: 1,
+    departure_location: "Paris",
+    arrival_location: "Marseille",
+    departure_time: "2023-03-10T10:00:00",
+    announcer: user,
+    available_seats: 2,
+    type: TripType.DRIVER,
+  },
+  {
+    id: 2,
+    departure_location: "Lyon",
+    arrival_location: "Nice",
+    departure_time: "2023-03-12T14:30:00",
+    announcer: user,
+    available_seats: 3,
+    type: TripType.PASSENGER,
+  },
+];
+</script>
 
 <template>
   <div>
-    <div class="w-full h-80 bg-content-flight">
+    <section class="w-full h-80 bg-content-flight">
       <div
         class="w-5/6 h-full mx-auto flex justify-center items-center flex flex-col"
       >
@@ -13,13 +48,15 @@
         </h1>
         <div class="bg-white w-full h-14 rounded-2xl shadow-md max-w-4xl"></div>
       </div>
-      <div>
-        <div>
-          <div>
-            <img src="@/assets/images/logos/logo.svg" alt="" />
-          </div>
-        </div>
+    </section>
+    <section class="p-14">
+      <Title class="mt-8" logoColor="base" content="Trajets à la une !" />
+      <div class="flex">
+        <template v-for="trip in trips" :key="trip.id">
+          <TripCard :trip="trip" />
+        </template>
       </div>
-    </div>
+      <Title class="mt-8" logoColor="base" content="Comment ça marche ?" />
+    </section>
   </div>
 </template>
