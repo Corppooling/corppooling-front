8c6cf9088448ee5019c1c4578c64abf5292079da
CesarWatrin <cesar.watrin@hotmail.com>
2023-03-09 18:09:32 +0100

    feat: carpooling tutorial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,46 @@ import Title from "@/components/atoms/Title.vue";
 import TripsSlider from "@/modules/trips/components/molecules/TripsSlider.vue";
 import HomeHeader from "@/components/molecules/HomeHeader.vue";
 import TripTypeSwitch from "@/modules/trips/components/molecules/TripTypeSwitch.vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { TripType } from "@/interfaces/trip.interface";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import search_driver from "@/assets/images/tutorial/search_driver.svg";
+import search_passenger from "@/assets/images/tutorial/search_passenger.svg";
+import talk_driver from "@/assets/images/tutorial/talk_driver.svg";
+import talk_passenger from "@/assets/images/tutorial/talk_passenger.svg";
+import car_driver from "@/assets/images/tutorial/car_driver.svg";
+import car_passenger from "@/assets/images/tutorial/car_passenger.svg";
+import { useI18n } from "vue-i18n";
 
 const type = ref<TripType>(TripType.DRIVER);
+
+const { t } = useI18n();
+
+const tutorialImages = computed((): Array<string> => {
+  switch (type.value) {
+    case TripType.PASSENGER:
+      return [search_passenger, talk_passenger, car_passenger];
+    default:
+      return [search_driver, talk_driver, car_driver];
+  }
+});
+
+const tutorialTexts = computed((): Array<string> => {
+  switch (type.value) {
+    case TripType.PASSENGER:
+      return [
+        t("home.tutorial.searchPassenger"),
+        t("home.tutorial.talkPassenger"),
+        t("home.tutorial.carPassenger"),
+      ];
+    default:
+      return [
+        t("home.tutorial.searchDriver"),
+        t("home.tutorial.talkDriver"),
+        t("home.tutorial.carDriver"),
+      ];
+  }
+});
 </script>
 
 <template>
@@ -35,6 +70,25 @@ const type = ref<TripType>(TripType.DRIVER);
             : $t("trip.searchDriver")
         }}
       </p>
+      <div class="flex justify-evenly w-full mt-14">
+        <template v-for="(image, index) in tutorialImages" :key="index">
+          <div class="flex flex-col h-52 w-80 flex justify-center items-center">
+            <div class="h-full my-auto">
+              <img class="h-full" :src="image" alt="" />
+            </div>
+            <div class="mt-4">
+              <p>{{ tutorialTexts[index] }}</p>
+            </div>
+          </div>
+          <hr
+            v-if="index < tutorialImages.length - 1"
+            class="border-none h-1 rounded-full w-24 my-auto mx-4"
+            :class="
+              type === TripType.DRIVER ? 'bg-main-base' : 'bg-content-glight'
+            "
+          />
+        </template>
+      </div>
     </section>
   </div>
 </template>
