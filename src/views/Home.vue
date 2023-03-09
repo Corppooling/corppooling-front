680a7d75265990784deb12cc8e38b3a45859ad56
CesarWatrin <cesar.watrin@hotmail.com>
2023-03-09 11:35:19 +0100

    feat: cut trips slider into component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,98 +1,6 @@
 <script lang="ts" setup>
-import { ref } from "vue";
 import Title from "@/components/atoms/Title.vue";
-import { type Trip, TripType } from "@/interfaces/trip.interface";
-import type { User } from "@/interfaces/user.interface";
-import TripCard from "@/modules/trips/components/molecules/TripCard.vue";
-import { Swiper, SwiperSlide } from "swiper/vue";
-import { type SwiperOptions, Navigation } from "swiper";
-import "swiper/css/effect-cards";
-import "swiper/css";
-import "swiper/css/navigation";
-
-const user: User = {
-  id: 1,
-  firstName: "Maxime",
-  lastName: "Didier",
-  email: "maxime.didier@aol.com",
-  profile_image:
-    "https://media.licdn.com/dms/image/C4E03AQFChS5oChTUeg/profile-displayphoto-shrink_400_400/0/1634940221677?e=1683763200&v=beta&t=l8TB14fChpB1l2Pr6ZcdmAlwR5UB2Ugj4IjihKMNUxQ",
-};
-
-const trips: Array<Trip> = [
-  {
-    id: 1,
-    departure_location: "Paris",
-    arrival_location: "Marseille",
-    departure_time: "2023-03-10T10:00:00",
-    announcer: user,
-    available_seats: 1,
-    type: TripType.DRIVER,
-  },
-  {
-    id: 2,
-    departure_location: "Lyon",
-    arrival_location: "Nice",
-    departure_time: "2023-03-12T14:30:00",
-    announcer: user,
-    available_seats: 3,
-    type: TripType.PASSENGER,
-  },
-  {
-    id: 3,
-    departure_location: "Paris",
-    arrival_location: "Marseille",
-    departure_time: "2023-03-10T10:00:00",
-    announcer: user,
-    available_seats: 1,
-    type: TripType.DRIVER,
-  },
-  {
-    id: 4,
-    departure_location: "Lyon",
-    arrival_location: "Nice",
-    departure_time: "2023-03-12T14:30:00",
-    announcer: user,
-    available_seats: 3,
-    type: TripType.PASSENGER,
-  },
-  {
-    id: 5,
-    departure_location: "Paris",
-    arrival_location: "Marseille",
-    departure_time: "2023-03-10T10:00:00",
-    announcer: user,
-    available_seats: 1,
-    type: TripType.DRIVER,
-  },
-  {
-    id: 6,
-    departure_location: "Lyon",
-    arrival_location: "Nice",
-    departure_time: "2023-03-12T14:30:00",
-    announcer: user,
-    available_seats: 3,
-    type: TripType.PASSENGER,
-  },
-];
-
-const swiperOptions = ref<SwiperOptions>({
-  modules: [Navigation],
-  centeredSlides: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  allowTouchMove: true,
-  observer: true,
-  observeParents: true,
-  breakpoints: {
-    0: {
-      slidesPerView: "auto",
-      spaceBetween: 26,
-    },
-  },
-});
+import TripsSlider from "@/modules/trips/components/molecules/TripsSlider.vue";
 </script>
 
 <template>
@@ -111,41 +19,7 @@ const swiperOptions = ref<SwiperOptions>({
     </section>
     <section class="py-8 px-6 md:px-12">
       <Title logoColor="base" :content="$t('home.popularTrips')" />
-      <Swiper
-        class="w-full"
-        :options="swiperOptions"
-        :modules="swiperOptions.modules"
-        :navigation="swiperOptions.navigation"
-        :breakpoints="swiperOptions.breakpoints"
-      >
-        <SwiperSlide
-          class="my-8 mx-auto sm:mx-0 max-w-full flex justify-center sm:max-w-fit"
-          v-for="trip in trips"
-          :key="trip.id"
-        >
-          <TripCard :trip="trip" />
-        </SwiperSlide>
-        <div>
-          <div
-            class="swiper-button-prev shadow-md rounded-full w-10 h-10 bg-white flex justify-center items-center after:content-none"
-          >
-            <font-awesome-icon
-              icon="fa-chevron-left"
-              size="xl"
-              class="text-content-base"
-            />
-          </div>
-          <div
-            class="swiper-button-next shadow-md rounded-full w-10 h-10 bg-white flex justify-center items-center after:content-none"
-          >
-            <font-awesome-icon
-              icon="fa-chevron-right"
-              size="xl"
-              class="text-content-base"
-            />
-          </div>
-        </div>
-      </Swiper>
+      <TripsSlider />
       <Title logoColor="base" :content="$t('home.howTo')" />
     </section>
   </div>
