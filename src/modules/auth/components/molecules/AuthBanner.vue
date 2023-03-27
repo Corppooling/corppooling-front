cc9c84279574c78dccd5de9d2d58fa5e66ac1509
CesarWatrin <cesar.watrin@hotmail.com>
2023-03-27 14:43:15 +0200

    fix: minor fixes

--- src/modules/auth/components/molecules/AuthBanner.vue
+++ src/modules/auth/components/molecules/AuthBanner.vue
@@ -4,7 +4,7 @@
   <div
     class="bg-content-glight hidden md:flex flex-col justify-end w-1/2 bg-[url('@/assets/images/login.svg')] bg-cover bg-contain bg-no-repeat bg-center"
   >
-    <div class="w-1/4 p-4 ml-auto">
+    <div class="w-44 p-4 ml-auto">
       <img src="@/assets/images/logos/logo_full.svg" alt="logo" />
     </div>
   </div>
