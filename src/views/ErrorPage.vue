86f7fd9e91ba0dfe8a648c3ac2016b67cf1aa41d
CesarWatrin <cesar.watrin@hotmail.com>
2023-05-29 14:11:12 +0200

    fix(error): css issue ios

--- src/views/ErrorPage.vue
+++ src/views/ErrorPage.vue
@@ -34,12 +34,12 @@ const buildTranslation = (type: 'title' | 'subtitle' | 'ban' | 'description') =>
     class="relative flex h-[calc(100vh-14.1rem)] w-full items-center justify-center bg-content-flight md:h-[calc(100vh-12.9rem)]"
   >
     <span
-      class="absolute left-0 top-0 flex h-full w-full select-none items-center justify-center text-[10rem] font-bold text-content-flight drop-shadow-neon-light sm:text-[20rem] lg:text-[30rem] xl:text-[40rem]"
+      class="absolute left-0 top-0 z-0 flex h-full w-full select-none items-center justify-center text-[10rem] font-bold text-content-flight drop-shadow-neon-light sm:text-[20rem] lg:text-[30rem] xl:text-[40rem]"
     >
       {{ codeError }}
     </span>
-    <div class="flex items-center justify-center text-center">
-      <div class="absolute flex flex-col gap-2 lg:gap-4">
+    <div class="relative z-10 flex items-center justify-center text-center">
+      <div class="flex flex-col gap-2 lg:gap-4">
         <h1 v-if="buildTranslation('title')" class="text-7xl">{{ buildTranslation('title') }}</h1>
         <h2 v-if="buildTranslation('subtitle')" class="text-3xl font-normal">
           {{ buildTranslation('subtitle') }}
