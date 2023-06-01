7dce37521be2a6573261e3980887eae70e771865
CesarWatrin <cesar.watrin@hotmail.com>
2023-06-01 09:27:36 +0200

    fix(company): minor fix

--- src/modules/account/views/Company.vue
+++ src/modules/account/views/Company.vue
@@ -10,9 +10,6 @@ const company = ref<Company | undefined>(userStore.user?.company);
 <template>
   <div v-if="company" class="w-full xl:w-96">
     <div class="flex">
-      <div class="flex w-40 items-center pr-8 mix-blend-color-burn">
-        <img class="object-contain" :src="company.logo" alt="company logo" />
-      </div>
       <div>
         <h3 class="mb-4 text-7xl">{{ company.name }}</h3>
         <div class="flex flex-col">
@@ -27,11 +24,14 @@ const company = ref<Company | undefined>(userStore.user?.company);
           </span>
         </div>
       </div>
+      <div class="flex w-40 items-center pl-8 mix-blend-color-burn">
+        <img class="object-contain" :src="company.logo" alt="company logo" />
+      </div>
     </div>
     <h3 class="my-8 text-2xl">{{ $t('account.myCompany.stats') }}</h3>
     <div class="flex w-full flex-col text-lg md:pl-4">
       <div class="flex justify-between py-2">
-        <span>{{ $t('account.myCompany.cluster') }}:</span>
+        <span>{{ $t('account.myCompany.activeUsers') }}:</span>
         <span class="ml-4 font-bold">{{ company.users?.length }}</span>
       </div>
       <div class="flex justify-between py-2">
