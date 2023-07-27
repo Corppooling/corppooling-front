3f27b0a5ea4a3e5d6ac1c03ea6a5acca1e867e9c
CesarWatrin <cesar.watrin@hotmail.com>
2023-07-27 22:20:11 +0200

    feat(stats): traduction

--- src/modules/account/components/organisms/ManageStats.vue
+++ src/modules/account/components/organisms/ManageStats.vue
@@ -4,6 +4,7 @@ import Chart from 'primevue/chart';
 import { Trip, TripType } from '@/interfaces/trip.interface';
 import Spinner from '@/components/atoms/Spinner.vue';
 import { ChartOptions } from 'chart.js';
+import { i18nGlobal } from '@/support/i18n';
 
 interface ChartData {
   labels: unknown[];
@@ -14,6 +15,7 @@ const props = defineProps<{
   trips: Trip[];
 }>();
 
+const { t } = i18nGlobal;
 const loading = ref<boolean>(true);
 const departureLocationsData = ref<ChartData>();
 const arrivalLocationsData = ref<ChartData>();
@@ -50,7 +52,7 @@ const setOccurrencesData = async (
     labels: occurrences.map((item) => item.name),
     datasets: [
       {
-        label: 'Nombre de trajets',
+        label: t('account.myManagement.statistics.tripsNumber'),
         data: occurrences.map((item) => item.occurrence),
         borderWidth: 1,
         backgroundColor: color,
@@ -61,10 +63,10 @@ const setOccurrencesData = async (
 
 const setTripsTypesData = async (): Promise<ChartData> => {
   return {
-    labels: ['Conducteur', 'Passager'],
+    labels: [t('trip.filter.driver'), t('trip.filter.passenger')],
     datasets: [
       {
-        label: 'Nombre de trajets',
+        label: t('account.myManagement.statistics.tripsNumber'),
         data: [
           props.trips.filter((trip) => trip.type === TripType.DRIVER).length,
           props.trips.filter((trip) => trip.type === TripType.PASSENGER).length,
@@ -78,18 +80,18 @@ const setTripsTypesData = async (): Promise<ChartData> => {
 
 const setTripsMembersData = async (): Promise<ChartData> => {
   const months = [
-    'Janvier',
-    'Février',
-    'Mars',
-    'Avril',
-    'Mai',
-    'Juin',
-    'Juillet',
-    'Août',
-    'Septembre',
-    'Octobre',
-    'Novembre',
-    'Décembre',
+    t('account.myManagement.statistics.months.january'),
+    t('account.myManagement.statistics.months.february'),
+    t('account.myManagement.statistics.months.march'),
+    t('account.myManagement.statistics.months.april'),
+    t('account.myManagement.statistics.months.may'),
+    t('account.myManagement.statistics.months.june'),
+    t('account.myManagement.statistics.months.july'),
+    t('account.myManagement.statistics.months.august'),
+    t('account.myManagement.statistics.months.september'),
+    t('account.myManagement.statistics.months.october'),
+    t('account.myManagement.statistics.months.november'),
+    t('account.myManagement.statistics.months.december'),
   ];
   const tripsMembersData: { month: string; members: number }[] = [];
   for (let i = 0; i < 12; i++) {
@@ -103,7 +105,7 @@ const setTripsMembersData = async (): Promise<ChartData> => {
     labels: tripsMembersData.map((item) => item.month),
     datasets: [
       {
-        label: 'Nombre de réservations',
+        label: t('account.myManagement.statistics.bookingsNumber'),
         data: tripsMembersData.map((item) => item.members),
         fill: false,
         borderWidth: 2,
@@ -143,8 +145,10 @@ const lineChartOptions = ref<ChartOptions>({});
 <template>
   <div v-if="!loading" class="flex flex-wrap justify-evenly gap-2">
     <div class="w-full 2xl:w-1/3">
-      <h3 class="text-lg">Villes de départs</h3>
-      <p class="mb-2 text-sm">Top 5 des villes de départ les plus fréquentes</p>
+      <h3 class="text-lg">{{ t('account.myManagement.statistics.departureTowns') }}</h3>
+      <p class="mb-2 text-sm">
+        {{ t('account.myManagement.statistics.departureTownsDescription') }}
+      </p>
       <Chart
         type="bar"
         :data="departureLocationsData"
@@ -153,23 +157,25 @@ const lineChartOptions = ref<ChartOptions>({});
       />
     </div>
     <div class="w-full 2xl:w-1/3">
-      <h3 class="text-lg">Villes d'arrivées</h3>
-      <p class="mb-2 text-sm">Top 5 des villes d'arrivées les plus fréquentes</p>
-      <Chart
-        type="bar"
-        :data="arrivalLocationsData"
-        class="h-72 w-full"
-        :options="barChartOptions"
-      />
+      <h3 class="text-lg">{{ t('account.myManagement.statistics.arrivalTowns') }}</h3>
+      <p class="mb-2 text-sm">
+        {{ t('account.myManagement.statistics.arrivalTownsDescription') }}
+        <Chart
+          type="bar"
+          :data="arrivalLocationsData"
+          class="h-72 w-full"
+          :options="barChartOptions"
+        />
+      </p>
     </div>
     <div class="w-full 2xl:w-1/3">
-      <h3 class="text-lg">Types de trajet</h3>
-      <p class="mb-2 text-sm">Répartition des types de trajet</p>
+      <h3 class="text-lg">{{ t('account.myManagement.statistics.tripTypes') }}</h3>
+      <p class="mb-2 text-sm">{{ t('account.myManagement.statistics.tripTypesDescription') }}</p>
       <Chart type="pie" :data="tripsTypesData" class="h-72 w-full" :options="pieChartOptions" />
     </div>
     <div class="w-full 2xl:w-1/3">
-      <h3 class="text-lg">Réservations</h3>
-      <p class="mb-2 text-sm">Répartition du nombre de réservations sur une année</p>
+      <h3 class="text-lg">{{ t('account.myManagement.statistics.tripBookings') }}</h3>
+      <p class="mb-2 text-sm">{{ t('account.myManagement.statistics.tripBookingsDescription') }}</p>
       <Chart type="line" :data="tripsMembersData" class="h-72 w-full" :options="lineChartOptions" />
     </div>
   </div>
