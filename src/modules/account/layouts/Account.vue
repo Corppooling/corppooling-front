16e77d5dbd2730798595e4a1151602df9a444cf4
CesarWatrin <cesar.watrin@hotmail.com>
2023-09-13 19:31:51 +0200

    fix(style): last try

--- src/modules/account/layouts/Account.vue
+++ src/modules/account/layouts/Account.vue
@@ -49,9 +49,7 @@ const currentLabel = computed<string>(() => {
 </script>
 
 <template>
-  <div
-    class="flex max-h-[calc(100vh)] flex-1 flex-col overflow-hidden md:md:max-h-[calc(100vh-5rem)] md:flex-row"
-  >
+  <div class="flex max-h-[calc(100vh-80px)] flex-1 flex-col overflow-hidden md:flex-row">
     <div class="order-2 flex-1 overflow-y-auto">
       <div class="p-10">
         <h1 v-if="currentLabel.trim()" class="text-4xl font-bold">{{ $t(currentLabel) }}</h1>
@@ -80,14 +78,14 @@ const currentLabel = computed<string>(() => {
       </div>
     </div>
     <div
-      class="sticky bottom-10 order-3 flex bg-content-flight text-content-base shadow-md md:hidden"
+      class="sticky bottom-0 order-3 flex bg-content-flight text-content-base shadow-md md:hidden"
     >
       <div class="flex w-full justify-between">
         <template v-for="tab in tabs" :key="tab.name">
           <RouterLink
             v-ripple
             :to="{ name: tab.name }"
-            class="flex w-full items-center justify-center pb-24 pt-4 hover:bg-content-xlight hover:text-white"
+            class="flex w-full items-center justify-center pb-8 pt-4 hover:bg-content-xlight hover:text-white"
             :class="[{ 'bg-content-xlight text-white': route.name === tab.name }, tab.class]"
           >
             <FontAwesomeIcon :icon="tab.icon" class="text-lg" />
